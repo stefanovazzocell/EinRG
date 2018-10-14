e1158b88eaea5fd09055ff729a2c3af1165cfdef
Ruiyu Gou <reyget42@gmail.com>
2018-10-14 10:53:12 -0700

    Updates

---
+++ precache-manifest.526d56a2316aa7b09b6395b8a73e4c48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a5e92e3751ba1fc362a14cf88d89076",
+    "url": "/EinRG/static/media/ein_transpa03.5.2a5e92e3.jpg"
+  },
+  {
+    "revision": "846c2733ec4dd80fca39",
+    "url": "/EinRG/static/js/runtime~main.846c2733.js"
+  },
+  {
+    "revision": "1bf65f1ccf9780bf28af",
+    "url": "/EinRG/static/js/main.1bf65f1c.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/EinRG/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "1bf65f1ccf9780bf28af",
+    "url": "/EinRG/static/css/main.78013acd.chunk.css"
+  },
+  {
+    "revision": "52ab6fdd9a4d3591a4a0cac64a2e9a66",
+    "url": "/EinRG/index.html"
+  }
+];
