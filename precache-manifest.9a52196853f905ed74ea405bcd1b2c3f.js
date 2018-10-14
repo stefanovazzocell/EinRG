7a1d605709db5d4aac7af9220181b28353fba6cc
Ruiyu Gou <reyget42@gmail.com>
2018-10-14 11:13:48 -0700

    Updates

---
+++ precache-manifest.9a52196853f905ed74ea405bcd1b2c3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a5e92e3751ba1fc362a14cf88d89076",
+    "url": "/static/media/ein_transpa03.5.2a5e92e3.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7995351f9e4859eb795f",
+    "url": "/static/js/main.7995351f.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "7995351f9e4859eb795f",
+    "url": "/static/css/main.78013acd.chunk.css"
+  },
+  {
+    "revision": "a0368f82909c4ae622c9066a5b3809aa",
+    "url": "/index.html"
+  }
+];
